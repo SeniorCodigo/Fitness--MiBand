fb6922be7ede11604af734d39aea827e0839e7ec
binaryoverload <alvarade.alain@gmail.com>
2020-05-13 11:15:02 -0700

    Save

--- Fitness/src/App.js
+++ Fitness/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import MedicionesScreen from "./components/dashboard/index";
 import ActividadesScreen from "./components/dashboard/actividades";
 import kalMedicScreen from "./components/dashboard/kcalMedic";
-import Yeah from "./components/dashboard/yeah";
 
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 import { createMaterialTopTabNavigator } from "react-navigation";
@@ -18,12 +17,10 @@ export default createMaterialTopTabNavigator(
     Mediciones: { screen: MedicionesScreen },
     Actividades: { screen: ActividadesScreen },
     Calorias: { screen: kalMedicScreen },
-    //Yeah: { screen: Yeah },
   },
   {
     intialRouteName: "Mediciones",
-    //order: ["Mediciones", "Actividades", "Calorias"],
+
     activeTintColor: "#33AFFF",
-    //shifting: true
   }
 );
